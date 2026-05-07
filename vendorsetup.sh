c24db1d66a11337fa06bc8e7ae746160fe2f7a66
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-05-07 04:15:31 +0300

    Add vendorsetup.sh for TWRP device tree

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_a14xm-user
+add_lunch_combo twrp_a14xm-userdebug
+add_lunch_combo twrp_a14xm-eng
+
