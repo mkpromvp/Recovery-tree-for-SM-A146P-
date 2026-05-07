9eca12002709c67652cd2b916b677a7efbe59783
AKRO <habiba31245@gmail.com>
2026-05-07 03:33:05 +0300

    add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a14xm.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a14xm-user \
+    twrp_a14xm-userdebug
