5f041d5e9a8d261e5534acd2a82217ffa5b6cce1
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-05-07 04:48:30 +0300

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a14xm.mk
+    $(LOCAL_DIR)/twrp_a146p.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a14xm-user \
-    twrp_a14xm-userdebug  \
-    twrp_a14xm-eng
+    twrp_a146p-user \
+    twrp_a146p-userdebug  \
+    twrp_a146p-eng
