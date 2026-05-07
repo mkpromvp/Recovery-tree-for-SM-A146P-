c9707bff7abb7aeb2a670d89e0178ae170e5b204
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-05-07 05:08:27 +0300

    Update DEVICE_PATH for Samsung device configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/a146p
+DEVICE_PATH := device/samsung/a14xm
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
